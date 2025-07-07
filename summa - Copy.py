ef3eaab7950a2db5e72e84bff49030a83ae12483
LOHESHANNAVI <144590049+LOHESHANNAVI@users.noreply.github.com>
2025-07-07 20:42:46 +0530

    DICT LOOPS

---
+++ summa - Copy.py
@@ -0,0 +1,46 @@
+company = {
+    "name": "TechCorp",
+    "employees": {
+        "emp1": {"name": "John", "role": "Developer"},
+        "emp2": {"name": "Sara", "role": "Designer"}
+    },
+    "location": "San Francisco"
+}
+
+a=[]
+a.append(company["employees"])
+# for i in company.items():
+#     print(i)
+#     a.append(i)
+print(a)    
+
+
+
+student = {
+    "name": "Bob",
+    "grades": [85, 92, 78],
+    "passed": True
+}
+
+student["grade"] = "A"
+print(student)
+
+
+books = {
+    "978-0143128540": {"title": "Sapiens", "author": "Yuval Noah Harari"},
+    "978-0385472579": {"title": "Zen and the Art of Motorcycle Maintenance", "author": "Robert M. Pirsig"},
+    "978-0062316110": {"title": "The Alchemist", "author": "Paulo Coelho"}
+}
+
+
+for key,book in books.items():
+
+    print(key,book)
+    print(f"{key} --by-- {book['title']} --and-- {book['author']} ")
+
+for book in books.values():
+
+    print(book)
+    print(f"{book['title']} --by-- book['author']")    
+
+    
